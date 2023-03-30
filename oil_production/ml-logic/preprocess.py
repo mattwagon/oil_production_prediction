f99ae15b4fa74d3a652ada77aa83304ebfebbd4c
Matthew Rowe <matthewjrowe01@gmail.com>
2023-03-30 08:18:10 +0100

    Changed some code based on new model

--- oil_production/ml-logic/preprocess.py
+++ oil_production/ml-logic/preprocess.py
@@ -2,49 +2,24 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-def preprocess_features():
+def preprocess_features(df):
 
     # Replace missing NAN with median
     for feature in df.columns:
         df[feature].replace(np.nan, df[feature].median(), inplace=True)
 
-    train_start_date = pd.to_datetime('2013-01-01')
-    train_end_date = pd.to_datetime('2014-11-30')
-    val_start_date = pd.to_datetime('2014-12-01')
-    val_end_date = pd.to_datetime('2014-12-31')
-    test_start_date = pd.to_datetime('2019-01-01')
+    train_start_date = pd.to_datetime('2007-02-01')
+    train_end_date = pd.to_datetime('2019-12-31')
+    test_start_date = pd.to_datetime('2020-01-01')
     test_end_date = pd.to_datetime('2022-12-31')
-    test2_start_date = pd.to_datetime('2009-01-01')
-    test2_end_date = pd.to_datetime('2009-01-21')
 
-    df_scale_fit = df[(df['Date'] >= train_start_date)
-                      & (df['Date'] <= val_end_date)]
-    df_train = df[(df['Date'] >= train_start_date)
-                  & (df['Date'] <= train_end_date)]
-    df_val = df[(df['Date'] >= val_start_date) & (df['Date'] <= val_end_date)]
-    df_test = df[(df['Date'] >= test_start_date)
-                 & (df['Date'] <= test_end_date)]
-    df_test2 = df[(df['Date'] >= test2_start_date)
-                  & (df['Date'] <= test2_end_date)]
+    df_train = df[(df['Date']>=train_start_date) & (df['Date']<=train_end_date)]
+    df_test = df[(df['Date']>=test_start_date) & (df['Date']<=test_end_date)]
 
-    # Instantiate MinMaxScaler
-    min_max_scaler = MinMaxScaler()
+    scaler_X = MinMaxScaler(feature_range=(0,1))
+    scaler_y = MinMaxScaler(feature_range=(0,1))
 
-    # Fit
-    min_max_scaler.fit(df_scale_fit.drop(columns=['Date','Qoil MPFM']))
-    X = df.drop(columns=['Date', 'Qoil MPFM'])
-    X_train = df_train.drop(columns=['Date', 'Qoil MPFM'])
-    X_val = df_val.drop(columns=['Date', 'Qoil MPFM'])
-    X_test = df_test.drop(columns=['Date', 'Qoil MPFM'])
-    X_test2 = df_test2.drop(columns=['Date', 'Qoil MPFM'])
-
-    # Transform
-    X_train_scaled = min_max_scaler.transform(X_train)
-    X_val_scaled = min_max_scaler.transform(X_val)
-    X_test_scaled = min_max_scaler.transform(X_test)
-    X_test2_scaled = min_max_scaler.transform(X_test2)
-
-    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns)
-    X_val_scaled = pd.DataFrame(X_val_scaled, columns=X_val.columns)
-    X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns)
-    X_test2_scaled = pd.DataFrame(X_test2_scaled, columns=X_test.columns)
+    X_train = scaler_X.fit_transform(X_train.reshape(-1,X_train.shape[-1])).reshape(X_train.shape)
+    X_test = scaler_X.transform(X_test.reshape(-1,X_test.shape[-1])).reshape(X_test.shape)
+    y_train = scaler_y.fit_transform(y_train.reshape(-1,y_train.shape[-1])).reshape(y_train.shape)
+    y_test = scaler_y.transform(y_test.reshape(-1,y_test.shape[1])).reshape(y_test.shape)
