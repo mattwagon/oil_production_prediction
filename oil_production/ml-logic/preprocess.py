5c38e363afd4e7fb4956093df02b1f48a519dfdc
Matthew Rowe <matthewjrowe01@gmail.com>
2023-03-30 10:19:02 +0100

    Almost completed subsample_squence, get_X_y and preprocess functions

--- oil_production/ml-logic/preprocess.py
+++ oil_production/ml-logic/preprocess.py
@@ -2,24 +2,44 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-def preprocess_features(df):
 
-    # Replace missing NAN with median
-    for feature in df.columns:
-        df[feature].replace(np.nan, df[feature].median(), inplace=True)
+def subsample_sequence(sequence, length, horizon):
 
-    train_start_date = pd.to_datetime('2007-02-01')
-    train_end_date = pd.to_datetime('2019-12-31')
-    test_start_date = pd.to_datetime('2020-01-01')
-    test_end_date = pd.to_datetime('2022-12-31')
+    # Bounds of sampling
+    last_possible = len(sequence) - length - horizon
+    random_start = np.random.randint(0, last_possible)
 
-    df_train = df[(df['Date']>=train_start_date) & (df['Date']<=train_end_date)]
-    df_test = df[(df['Date']>=test_start_date) & (df['Date']<=test_end_date)]
+    # Sample
+    X = sequence.iloc[random_start:random_start + length, :].drop(columns=['Date'])
+    y = sequence.iloc[random_start + length:random_start + length + horizon, :]['Qoil MPFM']
+
+    return X, y
+
+def preprocess_features(df_train):
+
+    def get_X_y(sequence, length, horizon, number_of_samples):
+
+        X, y = [], []
+
+        # Do as many samples as specified
+        for sample in range(1, number_of_samples + 1):
+
+            # Record sample X & y
+            xi, yi = subsample_sequence(sequence, length, horizon)
+            X.append(np.array(xi.values.T.tolist()).T) # Getting the right shape (sequences, observations,features)
+            y.append(yi)
+
+        return np.array(X), np.array(y)
+
+    X_train, y_train = get_X_y(sequence=df_train,
+               length=500,
+               horizon=1,
+               number_of_samples=5000)
 
     scaler_X = MinMaxScaler(feature_range=(0,1))
     scaler_y = MinMaxScaler(feature_range=(0,1))
 
     X_train = scaler_X.fit_transform(X_train.reshape(-1,X_train.shape[-1])).reshape(X_train.shape)
-    X_test = scaler_X.transform(X_test.reshape(-1,X_test.shape[-1])).reshape(X_test.shape)
     y_train = scaler_y.fit_transform(y_train.reshape(-1,y_train.shape[-1])).reshape(y_train.shape)
-    y_test = scaler_y.transform(y_test.reshape(-1,y_test.shape[1])).reshape(y_test.shape)
+
+    return X_train, y_train
