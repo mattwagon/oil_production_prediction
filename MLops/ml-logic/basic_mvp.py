16923f8be6d4aabd72b31499f554c2efe9f9d7e7
Matthew Rowe <matthewjrowe01@gmail.com>
2023-03-30 07:29:07 +0100

    Organised requirements file to contain only needed packages

--- MLops/ml-logic/basic_mvp.py
+++ MLops/ml-logic/basic_mvp.py
@@ -1,15 +1,8 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_validate
 
 def basic_mvp():
 
-    baseline_model = LinearRegression()
-
-    baseline_model.fit(X_train_scaled, y_train)
-
-    baseline_pred_val = baseline_model.predict(X_val_scaled)
-    baseline_pred_test = baseline_model.predict(X_test_scaled)
-    baseline_pred_test2 = baseline_model.predict(X_test2_scaled)
-    baseline_mae_val = np.mean(np.abs(baseline_pred_val - y_val), axis=0)
-    baseline_mae_test = np.mean(np.abs(baseline_pred_test - y_test), axis=0)
-    baseline_mae_test2 = np.mean(np.abs(baseline_pred_test2 - y_test2), axis=0)
+    train_start_date = pd.to_datetime('2007-02-01')
+    train_end_date = pd.to_datetime('2019-12-31')
+    test_start_date = pd.to_datetime('2020-01-01')
+    test_end_date = pd.to_datetime('2022-12-31')
